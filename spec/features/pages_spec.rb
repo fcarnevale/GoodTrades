05baed664494bd617278b9e9bc4581035c45623a
Frank Carnevale <frank@fourward.org>
2014-06-15 13:56:01 -0400

    Refactor test description

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'Site Pages' do
   describe 'Home Page' do
-    it 'has the right content' do
+    it 'has the name of the site' do
       visit root_path
       expect(page).to have_content 'Good Trades'
     end
